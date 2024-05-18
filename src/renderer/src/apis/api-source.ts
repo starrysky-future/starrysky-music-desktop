6e44a36ebb4e5b389815d4e5249935c4a6746a68
shuguokun <shuguokun@123456.com>
2024-05-18 17:45:28 +0800

    删除test-api

--- src/renderer/src/apis/api-source.ts
+++ src/renderer/src/apis/api-source.ts
@@ -9,9 +9,9 @@ import api_mg_local from './mg/api-local';
 import api_wy_local from './wy/api-local';
 
 const setStore = useSetStore(pinia);
-const { setList, apiSourceList } = storeToRefs(setStore);
+const { setList } = storeToRefs(setStore); // apiSourceList
 
-let allApi = {
+const allApi = {
   local_tx: api_tx_local,
   local_kg: api_kg_local,
   local_kw: api_kw_local,
@@ -19,25 +19,25 @@ let allApi = {
   local_wy: api_wy_local
 };
 
-try {
-  const api_test_tx = await import('./tx/api-test');
-  const api_test_kg = await import('./kg/api-test');
-  const api_test_kw = await import('./kw/api-test');
-  const api_test_mg = await import('./mg/api-test');
-  const api_test_wy = await import('./wy/api-test');
+// try {
+//   const api_test_tx = await import('./tx/api-test');
+//   const api_test_kg = await import('./kg/api-test');
+//   const api_test_kw = await import('./kw/api-test');
+//   const api_test_mg = await import('./mg/api-test');
+//   const api_test_wy = await import('./wy/api-test');
 
-  const testApi = {
-    test_tx: api_test_tx.default,
-    test_kg: api_test_kg.default,
-    test_kw: api_test_kw.default,
-    test_mg: api_test_mg.default,
-    test_wy: api_test_wy.default
-  };
-  allApi = Object.assign(allApi, testApi);
-  apiSourceList.value.push({ name: '测试音源', id: 'test' });
-} catch (error) {
-  console.log(error);
-}
+//   const testApi = {
+//     test_tx: api_test_tx.default,
+//     test_kg: api_test_kg.default,
+//     test_kw: api_test_kw.default,
+//     test_mg: api_test_mg.default,
+//     test_wy: api_test_wy.default
+//   };
+//   allApi = Object.assign(allApi, testApi);
+//   apiSourceList.value.push({ name: '测试音源', id: 'test' });
+// } catch (error) {
+//   console.log(error);
+// }
 
 const getAPI = (source) => allApi[`${setList.value.apiSource}_${source}`];
 
